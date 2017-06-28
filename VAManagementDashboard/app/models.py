2142da689fae4ab12df34248ceed102914c1e106
mihailnacev <m.nacev@yahoo.com>
2017-06-28 10:09:56 +0200

    Initial commit

---
+++ VAManagementDashboard/app/models.py
@@ -0,0 +1,112 @@
+from django.db import models
+from django.core.validators import RegexValidator
+from django import forms
+# Create your models here.
+
+
+class Company(models.Model):
+    class Meta:
+        verbose_name='Company'
+        verbose_name_plural='Companies'
+
+    Name=models.CharField(max_length=50)
+    Description=models.CharField(max_length=200)
+    Address=models.CharField(max_length=150)
+    ContactPerson=models.CharField(max_length=100)
+    ContactEmail=models.EmailField()
+    ContactNumber=models.CharField(
+        max_length=9,
+        blank=True,
+        validators=[
+            RegexValidator(
+                regex='07(0|1|2|5|6|7|8)\d{6}',
+                message='Not valid format',
+                code='invalid_ContactNumber'
+            ),
+        ]
+    )
+
+    def __repr__(self):
+        return "Name: %s, Address: %s" %(self.Name, self.Address)
+
+    def __str__(self):
+        return "Name: %s, Address: %s" % (self.Name, self.Address)
+
+    def to_json(self):
+        json_data = {
+            "Name" : self.Name,
+            "Description": self.Description,
+            "Address": self.Description,
+            "ContactPerson": self.ContactPerson,
+            "ContactEmail": self.ContactEmail,
+            "ContactNumber": self.ContactNumber,
+        }
+        return json_data
+
+#kobjekt.__dict__
+class DataCenter(models.Model):
+    Name=models.CharField(max_length=100)
+    Description=models.CharField(max_length=200)
+    KNET_ENUM=0
+    BALKANTEL_ENUM=1
+    CUSTOMER_ENUM=2
+    Locations=['Knet','Balkantel','Customer']
+    LOCATION_CHOICES=(
+        (KNET_ENUM,'Knet'),
+        (BALKANTEL_ENUM,'Balkantel'),
+        (CUSTOMER_ENUM,'Customer'),
+    )
+    Location=models.SmallIntegerField(choices=LOCATION_CHOICES)
+    URL=models.URLField()
+    VM_ENUM=0
+    CUSTOMER_ENUM_ONE=1
+    LXC_ENUM=2
+    Types=['VM', 'Customer', 'LXC']
+    TYPE_CHOICES=(
+        (VM_ENUM,'VM'),
+        (CUSTOMER_ENUM_ONE,'Customer'),
+        (LXC_ENUM,'LXC'),
+    )
+    Type=models.SmallIntegerField(choices=TYPE_CHOICES)
+
+    def __str__(self):
+        return "Name: %s, Location: %s, URL: %s, Type; %s" %(self.Name,self.Location,self.URL,self.Type)
+
+    def __repr__(self):
+        return "Name: %s, Location: %s, URL: %s, Type; %s" % (self.Name, self.Location, self.URL, self.Type)
+
+    def to_json(self):
+        json_data={
+          'Name': self.Name,
+          'Description': self.Description,
+          'Location': self.Locations[int(self.Location)],
+          'URL': self.URL,
+          'Type': self.Types[self.Type],
+        };
+        return json_data
+
+
+class VAMaster(models.Model):
+    Domain=models.CharField(
+        max_length=100,
+        blank=True,
+        validators=[
+            RegexValidator(
+                regex='^[a-z0-9]+(\.[a-z0-9]+)+$',
+                message='Not valid format',
+                code='invalid_Domain'
+            ),
+        ]
+    )
+    URL=models.URLField()
+    InternalIP=models.GenericIPAddressField()
+    Username=models.CharField(max_length=100)
+    Password = models.CharField(max_length=20,null=True)
+    VPNPort=models.IntegerField()
+    #Company=models.OneToOneField(Company,null=True,on_delete=models.SET_NULL)
+    Company=models.ForeignKey('Company',null=True,on_delete=models.SET_NULL)
+    DataCenter=models.ForeignKey('DataCenter',null=True)
+
+
+
+
---
+++ VAManagementDashboard/app/views.py
@@ -0,0 +1,36 @@
+from django.shortcuts import render
+from django.http import Http404
+import json
+# Create your views here.
+from django.http import HttpResponseRedirect, HttpResponse, JsonResponse
+from django.core.urlresolvers import reverse
+from django.template import loader
+from .models import Company, DataCenter, VAMaster
+from django.core import serializers
+
+
+
+def get_all_companies(request):
+    # data=serializers.serialize('json',Company.objects.all())
+    # data= json.dumps(Company.objects.all())
+    data_json=[]
+    for company in Company.objects.all():
+        data_json.append(json.dumps(company.to_json()))
+    data= json.dumps(data_json)
+    return JsonResponse(data, safe=False)
+def get_company(request,company_id):
+    company =  Company.objects.get(pk=company_id).to_json()
+    company = json.dumps(company)
+    return JsonResponse(company, safe=False)
+    # return HttpResponse(Company.objects.get(pk=1))
+def get_all_dataCenters(request):
+    data=serializers.serialize('json',DataCenter.objects.all())
+    return JsonResponse(data,safe=False)
+def get_dataCenter(request,dataCenter_id):
+   # data=serializers.serialize('json', [DataCenter.objects.get(pk=dataCenter_id)])
+   dataCenter= DataCenter.objects.get(pk=dataCenter_id).to_json()
+   dataCenter= json.dumps(dataCenter)
+   return JsonResponse(dataCenter, safe=False)
+def get_all_VAMasters(request):
+    data=serializers.serialize('json', VAMaster.objects.select_related().all())
+    return JsonResponse(data,safe=False)
